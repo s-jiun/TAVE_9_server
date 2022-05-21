2c07d19f1129b1dfe055aea43a945b9d1a584e90
Jiun <jiun48562@gmail.com>
2022-05-21 16:58:27 +0900

    first setting

---
+++ springboot/src/main/java/com/tave_app_1/senapool/SenapoolApplication.java
@@ -0,0 +1,13 @@
+package com.tave_app_1.senapool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenapoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenapoolApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/tave_app_1/senapool/SenapoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave_app_1.senapool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenapoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
