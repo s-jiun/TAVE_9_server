1b3612e015c195d4ffd2f0ff7d1428cb0424b13d
Jiun <jiun48562@gmail.com>
2022-06-23 16:29:55 +0900

    [ADD] Likes Entity

---
+++ springboot/src/main/java/com/tave_app_1/senapool/entity/Likes.java
@@ -0,0 +1,28 @@
+@Entity
+@Getter
+@Setter
+@Table(name = "like_table")
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "likePK")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userPK")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diaryPK")
+    private Diary diary;
+
+    public static Likes setLike(User user, Diary diary){
+        Likes like = new Likes();
+        like.setUser(user);
+        like.setDiary(diary);
+
+        return like;
+    }
+
+
+}
