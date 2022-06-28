2aba1849edd548c92cbd432b1c29913cdc58c739
Jiun <jiun48562@gmail.com>
2022-06-29 00:40:37 +0900

    [FIX] Likes entity

--- springboot/src/main/java/com/tave_app_1/senapool/entity/Likes.java
+++ springboot/src/main/java/com/tave_app_1/senapool/entity/Likes.java
@@ -1,6 +1,5 @@
 package com.tave_app_1.senapool.entity;
 
-import com.tave_app_1.senapool.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,19 +17,17 @@ public class Likes {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userPK")
-    private User user;
+    private User User;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "diaryPK")
-    private Diary diary;
+    private PlantDiary PlantDiary;
 
-    public static Likes setLike(User user, Diary diary){
+    public static Likes setLike(User user, PlantDiary diary){
         Likes like = new Likes();
         like.setUser(user);
-        like.setDiary(diary);
+        like.setPlantDiary(diary);
 
         return like;
     }
-
-
 }
