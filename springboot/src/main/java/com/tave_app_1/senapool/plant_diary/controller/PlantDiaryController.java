9807c68ccb22618927a60dc45fe5ca31cd156ec7
Seo Kang Deok <rkdejr2321@naver.com>
2022-07-11 14:22:48 +0900

    [MODIFY] 다이러리 컨트롤러 수정

--- springboot/src/main/java/com/tave_app_1/senapool/plant_diary/controller/PlantDiaryController.java
+++ springboot/src/main/java/com/tave_app_1/senapool/plant_diary/controller/PlantDiaryController.java
@@ -4,7 +4,6 @@ import com.tave_app_1.senapool.entity.MyPlant;
 import com.tave_app_1.senapool.entity.User;
 import com.tave_app_1.senapool.exception.ErrorCode;
 import com.tave_app_1.senapool.exception.ErrorResponse;
-import com.tave_app_1.senapool.myplant_list.dto.diary_list_response.DiaryListResponseDto;
 import com.tave_app_1.senapool.myplant_list.repository.MyPlantRepository;
 import com.tave_app_1.senapool.plant_diary.dto.PlantDiaryDetailDto;
 import com.tave_app_1.senapool.plant_diary.dto.PlantDiaryUpdateDto;
@@ -13,16 +12,8 @@ import com.tave_app_1.senapool.plant_diary.service.PlantDiaryService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.beanvalidation.CustomValidatorBean;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
@@ -48,8 +39,14 @@ public class PlantDiaryController {
             // 인증 성공
             if (user.getUserPK() == userPK) {
                 MyPlant myPlant = myPlantRepository.findByPlantPK(plantPK);
-                plantDiaryService.uploadDiary(plantDiaryUploadDto, user, myPlant);
-                return new ErrorResponse<>(ErrorCode.SUCCESS);
+                if (myPlant.getPlantPK() == plantPK){
+                    plantDiaryService.uploadDiary(plantDiaryUploadDto, user, myPlant);
+                    return new ErrorResponse<>(ErrorCode.SUCCESS);
+                }
+                else{
+                    return new ErrorResponse<>(ErrorCode.INVALID_REQUEST);
+                }
+
             }
             // 인증 실패
             else {
@@ -65,7 +62,7 @@ public class PlantDiaryController {
     @PutMapping("/myplant-diary/{userPK}/{diaryPK}")
     public ErrorResponse<?> plantDiaryUpdate(@PathVariable("userPK") Long userPK,@PathVariable("diaryPK") Long diaryPK,
                                              @Valid PlantDiaryUpdateDto plantDiaryUpdateDto,
-                                         @ApiIgnore Authentication authentication){
+                                             @ApiIgnore Authentication authentication){
 
         try {
 
@@ -91,7 +88,7 @@ public class PlantDiaryController {
     @ApiOperation(value = "식물 일기 삭제")
     @DeleteMapping("/myplant-diary/{userPK}/{diaryPK}")
     public ErrorResponse<?> plantDiaryDelete(@PathVariable("userPK") Long userPK,@PathVariable("diaryPK") Long diaryPK,
-                                         @ApiIgnore Authentication authentication){
+                                             @ApiIgnore Authentication authentication){
 
         try {
             User user = (User) authentication.getPrincipal();
